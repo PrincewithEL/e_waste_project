f8714a3b218f9a2a522b8053b3cdb37bcbf47695
PrincewithEL <86285043+PrincewithEL@users.noreply.github.com>
2024-10-05 17:40:30 +0300

    Ninth Commit

--- E_Waste_Project/settings.py
+++ E_Waste_Project/settings.py
@@ -152,16 +152,12 @@ USE_L10N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [BASE_DIR / 'static']
-
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),  # Add your static files directory
-# ]
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
@@ -171,3 +167,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
